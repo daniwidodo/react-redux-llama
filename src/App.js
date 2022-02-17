3bf53057d1f92065b28160df675fc49047bbc977
bangtur_studio <dani.widodo87@gmail.com>
2022-02-17 21:11:01 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+
+  }
+
+  console.log(name, email);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <nav className='navigation'> username is ...</nav>
+      <div className="user-form">
+        
+        <form >
+          <label >username :</label>
+          <input type="text" placeholder='username' onChange={ (e) => setName(e.target.value) } />
+          <br />
+
+          <label >email :</label>
+          <input type="text" placeholder='email@gmail.com' onChange={ (e) => setEmail(e.target.value) } />
+          <br />
+
+          <button onClick={handleClick}>submit</button>
+        </form>
+      </div>
     </div>
   );
 }
